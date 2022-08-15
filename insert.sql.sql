990f74e9dcbf66398496c1cea5fe285cd3c18536
Silvina-Dew <106364381+Silvina-Dew@users.noreply.github.com>
2022-08-14 22:11:57 -0300

    Insertar datos a bd

---
+++ insert.sql.sql
@@ -0,0 +1,62 @@
+insert into Dueno(DNI ,Nombre,Apellido ,Telefono ,Direccion)
+values(33646870,"Pancho","Marck",49856634,"fuego 1254");
+
+insert into Dueno(DNI ,Nombre,Apellido ,Telefono ,Direccion)
+values(34564564,"Jorge","Paul",49856634,"Telar 1254");
+
+insert into Dueno(DNI ,Nombre,Apellido ,Telefono ,Direccion)
+values(5486754,"Lucas","Domingo",49856634,"Luz 9254");
+
+insert into Dueno(DNI ,Nombre,Apellido ,Telefono ,Direccion)
+values(11583758,"Raul","Paredes",49856634,"carbon 1564");
+
+insert into Dueno(DNI ,Nombre,Apellido ,Telefono ,Direccion)
+values(27438569,"Raul","Paredes",49856634,"bajada 1564");
+
+insert into Dueno(DNI ,Nombre,Apellido ,Telefono ,Direccion)
+values(25956234,"Rocio","Pardo",49856634,"Pasaje 7564");
+
+insert into Dueno(DNI ,Nombre,Apellido ,Telefono ,Direccion)
+values(30756348,"Raul","Giro",49856634,"Rosa 5664");
+
+insert into Perro(ID_Perro,Nombre ,Fecha_nac ,sexo ,DNI_dueno)
+values("1","Poroto","2000-08-12","macho",34564564);
+
+insert into Perro(ID_Perro,Nombre ,Fecha_nac ,sexo ,DNI_dueno)
+values("2","Roco","2006-02-12","macho",5486754);
+
+insert into Perro(ID_Perro,Nombre ,Fecha_nac ,sexo ,DNI_dueno)
+values("3","koka","2000-08-12","hembra",11583758);
+
+insert into Perro(ID_Perro,Nombre ,Fecha_nac ,sexo ,DNI_dueno)
+values("4","Pol","2000-08-12","macho",27438569);
+
+insert into Perro(ID_Perro,Nombre ,Fecha_nac ,sexo ,DNI_dueno)
+values("5","Luna","2000-08-12","hembra",25956234);
+
+insert into Perro(ID_Perro,Nombre ,Fecha_nac ,sexo ,DNI_dueno)
+values("6","Paki","2000-08-12","macho",30756348);
+
+insert into Perro(ID_Perro,Nombre ,Fecha_nac ,sexo ,DNI_dueno)
+values("7","Lobo","2022-07-12","macho",33646870);
+
+insert into Historial(ID_Historial, Fecha, Perro, Descripcion, monto)
+VALUES(1, '2022-07-12', 1, 'Saludable', 22000);
+
+insert into Historial(ID_Historial, Fecha, Perro, Descripcion, monto)
+VALUES(2,'2006-02-12', 2, 'Saludable', 7000);
+
+insert into Historial(ID_Historial, Fecha, Perro, Descripcion, monto)
+VALUES(3, '2000-08-12', 3, 'Saludable', 9000);
+
+insert into Historial(ID_Historial, Fecha, Perro, Descripcion, monto)
+VALUES(4, '2000-08-12', 4, 'Saludable', 3560);
+
+insert into Historial(ID_Historial, Fecha, Perro, Descripcion, monto)
+VALUES(5, '2000-07-22', 5, 'Saludable', 12000);
+
+insert into Historial(ID_Historial, Fecha, Perro, Descripcion, monto)
+VALUES(6, '2000-07-07', 6, 'Saludable', 5000);
+
+insert into Historial(ID_Historial, Fecha, Perro, Descripcion, monto)
+VALUES(7, '2022-07-09', 7, 'Saludable', 11000);
